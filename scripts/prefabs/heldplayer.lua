e947b72f801c58baed4626f519e9fe92c3b6e18d
Skylarr <theotherskylarr@gmail.com>
2023-05-04 06:25:56 -0700

    Cleanup old bad code

--- scripts/prefabs/heldplayer.lua
+++ scripts/prefabs/heldplayer.lua
@@ -2,38 +2,27 @@ local assets = {
 
 }
 
-local prefabs = {
-} 
-
-local holder = nil 
-
 local function onequip(inst, owner)
     
     owner.AnimState:Show("ARM_carry")
     owner.AnimState:Hide("ARM_normal")
-    holder = owner
 end
  
 local function onunequip(inst, owner)
     owner.AnimState:Hide("ARM_carry")
     owner.AnimState:Show("ARM_normal")
 
+    -- if we get dropped as an "item" rather than a projectile, get removed
     inst:DoTaskInTime(0, function(inst)
         if not inst:HasTag("NOCLICK") then
-            owner:PushEvent("stopholding")
-            inst:Remove()
+            inst:DoTaskInTime(0, inst:Remove())
         end
     end)
 end
  
-local function onthrown(inst)
+local function OnThrown(inst)
     inst:AddTag("NOCLICK")
     inst.persists = false
-    
-    -- inst.AnimState:PlayAnimation("spin_loop", true)
-
-    inst.entity:AddDynamicShadow()
-    inst.DynamicShadow:SetSize(1, 1)
     
     inst.Physics:SetMass(1)
     inst.Physics:SetCapsule(0.2, 0.2)
@@ -45,39 +34,23 @@ local function onthrown(inst)
     inst.Physics:CollidesWith(COLLISION.OBSTACLES)
     inst.Physics:CollidesWith(COLLISION.ITEMS)
 
-    if holder then
-        holder:PushEvent("playerthrown", {target = inst.components.complexprojectile.targetpos, item = inst})
+    local owner = inst.components.inventoryitem:GetGrandOwner()
+    if owner then
+        owner:PushEvent("playerthrown", {target = inst.components.complexprojectile.targetpos, item = inst})
     end
 end
 
 local function OnHit(inst)
-    inst:DoTaskInTime(1, inst:Remove())
+    inst:DoTaskInTime(0, inst:Remove())
 end
 
-local function common_fn(bank, build, anim, tag, isinventoryitem)
+local function fn()
     local inst = CreateEntity()
  
     inst.entity:AddTransform()
-    inst.entity:AddAnimState()
-    inst.entity:AddSoundEmitter()
     inst.entity:AddNetwork()
  
-    if isinventoryitem then
-        MakeInventoryPhysics(inst)
-    else
-        inst.entity:AddPhysics()
-        inst.Physics:SetMass(1)
-        inst.Physics:SetCapsule(0.2, 0.2)
-        inst.Physics:SetFriction(0)
-        inst.Physics:SetDamping(0)
-        inst.Physics:SetCollisionGroup(COLLISION.CHARACTERS)
-        inst.Physics:ClearCollisionMask()
-        inst.Physics:CollidesWith(COLLISION.WORLD)
-    end
- 
-    if tag ~= nil then
-        inst:AddTag(tag)
-    end
+    MakeInventoryPhysics(inst)
  
     inst.entity:SetPristine()
  
@@ -85,39 +58,21 @@ local function common_fn(bank, build, anim, tag, isinventoryitem)
         return inst
     end
  
-    inst:AddComponent("locomotor")
- 
-	inst:AddComponent("complexprojectile")
- 
-    return inst
-end
-
-local function fn()
-    local inst = common_fn("kidpotion", "kidpotion", "idle", "projectile", true)
-
-    if not TheWorld.ismastersim then
-        return inst
-    end
- 
-    inst:AddComponent("inspectable")
- 
     inst:AddComponent("inventoryitem")
 	inst.components.inventoryitem.cangoincontainer = false
-	
-	inst.components.complexprojectile:SetHorizontalSpeed(16)
-	inst.components.complexprojectile:SetGravity(-50)
-	inst.components.complexprojectile:SetLaunchOffset(Vector3(.25, 1, 0))
-	inst.components.complexprojectile:SetOnLaunch(onthrown)
-    inst.components.complexprojectile:SetOnHit(OnHit)
 	
     inst:AddComponent("equippable")
     inst.components.equippable:SetOnEquip(onequip)
     inst.components.equippable:SetOnUnequip(onunequip)
+
+	inst:AddComponent("complexprojectile")
+    inst.components.complexprojectile:SetHorizontalSpeed(16)
+	inst.components.complexprojectile:SetGravity(-50)
+	inst.components.complexprojectile:SetLaunchOffset(Vector3(.25, 1, 0))
+	inst.components.complexprojectile:SetOnLaunch(OnThrown)
+    inst.components.complexprojectile:SetOnHit(OnHit)
 	
     return inst
 end
 
-STRINGS.NAMES.HELDPLAYER = "Held Player"
-STRINGS.CHARACTERS.GENERIC.DESCRIBE.HELDPLAYER = "Caught ya!"
-
-return Prefab("heldplayer", fn, assets, prefabs)
+return Prefab("heldplayer", fn)
